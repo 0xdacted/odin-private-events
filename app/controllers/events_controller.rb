d09e32610021cd139605182ed4db4c5fc3fb9ba4
Red <crabmarket56@gmail.com>
2022-09-27 11:46:28 +0900

    Add event creation in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,13 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    @event = @user.events.build(event_params)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :location, :date)
+  end
 end
