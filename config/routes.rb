8e3122cbda292c58f5539ab7f605da64615d5087
Red <crabmarket56@gmail.com>
2022-09-27 11:23:54 +0900

    Add user show page w events created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   get 'event/index'
+  get 'users/:id' => 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
