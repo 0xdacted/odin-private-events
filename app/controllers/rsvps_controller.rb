151020b95ce443fc62df80c0faedf9f3662842a8
Red <crabmarket56@gmail.com>
2022-09-29 16:22:50 +0900

    Draft cancel rsvp logic

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -4,6 +4,13 @@ class RsvpsController < ApplicationController
     @event = Event.find(params[:id])
     @event.attendees << current_user
     current_user.attended_events << @event
+    redirect_to 'events#index'
+  end
+
+  def cancel_rsvp
+    @event = Event.find(params[:id]) 
+    @event.attendees.delete(curent_user)
+    redirect_to 'events#index'
   end
  
 end
